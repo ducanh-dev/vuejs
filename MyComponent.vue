c91cd7ffc2e63de5a3e136eb2dea005c33bea78f
nguyenducanh <ducanh.dev@outlook.com.vn>
2020-01-03 03:16:11 +0700

    commit component tutorial

---
+++ MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="my-component">This is my component</div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: red;
+    }
+</style>
